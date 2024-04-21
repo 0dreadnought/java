d0e8daf9d1243911e163f3999c47e50dd86b52f9
Ilyas Utenov <1ukrainech1@gmail.com>
2024-04-21 15:24:46 +0500

    giga_chad_token

---
+++ mcmod/src/main/java/net/dread/playGround/item/ModCreativeModTabs.java
@@ -0,0 +1,26 @@
+package net.dread.playGround.item;
+
+import net.dread.playGround.playGround;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModCreativeModTabs {
+
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, playGround.MODID);
+
+    public static final RegistryObject<CreativeModeTab> PlayGround_TAB = CREATIVE_MODE_TABS.register("playground",
+            ()->CreativeModeTab.builder().icon(()->new ItemStack(ModItems.GIGA_CHAD_TOKEN.get()))
+            .title(Component.translatable("creativetab.playground"))
+            .displayItems((pParameters, pOutput)->{
+                pOutput.accept(ModItems.GIGA_CHAD_TOKEN.get());
+            }).build());
+
+    public static void register (IEventBus eventBus){
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
---
+++ mcmod/src/main/java/net/dread/playGround/item/ModItems.java
@@ -0,0 +1,20 @@
+package net.dread.playGround.item;
+
+import net.dread.playGround.playGround;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, playGround.MODID);
+
+    public static final RegistryObject<Item> GIGA_CHAD_TOKEN = ITEMS.register("token",()->new Item(new Item.Properties()));
+
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
