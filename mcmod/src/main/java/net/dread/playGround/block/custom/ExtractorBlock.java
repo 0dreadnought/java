1431742c2867cf8dd8f43a44166e9338e74ecabe
Ilyas Utenov <1ukrainech1@gmail.com>
2024-05-12 21:46:53 +0500

    tags

--- mcmod/src/main/java/net/dread/playGround/block/custom/ExtractorBlock.java
+++ mcmod/src/main/java/net/dread/playGround/block/custom/ExtractorBlock.java
@@ -1,43 +1,53 @@
 package net.dread.playGround.block.custom;
 
-import net.dread.playGround.block.ModBlocks;
+
 import net.dread.playGround.item.ModItems;
+import net.dread.playGround.tags.ModTags;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
-import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.entity.FurnaceBlockEntity;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.common.Tags;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class ExtractorBlock extends Block{
+
+
+
 
     public ExtractorBlock(Properties pProperties) {
         super(pProperties);
     }
 
     public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
-        if(pPlayer.getItemInHand(pHand).getItem()== ModBlocks.JADE_ORE_BLOCK.get().asItem()){
+
+        String item_name = pPlayer.getItemInHand(pHand).getDescriptionId();
+
+        if(pPlayer.getItemInHand(pHand).is(ModTags.Items.EXTRACTOR_INPUT)){
             pLevel.playSound(pPlayer,pPos,SoundEvents.UI_STONECUTTER_TAKE_RESULT,SoundSource.BLOCKS,3f,1f);
             pPlayer.getItemInHand(pHand).shrink(1);
-            Random rnd = new Random();
-            for (int i = 0;i<=rnd.nextInt(4);i++) {
-                pPlayer.addItem(ModItems.JADE.get().getDefaultInstance());
+            Random rnd = new Random(pLevel.getGameTime());
+            int o = 1;
+            int b = 4;
+            if (pPlayer.getItemInHand(pHand).is(Tags.Items.ORES_LAPIS)){b=16;o=7;}
+            if (pPlayer.getItemInHand(pHand).is(Tags.Items.ORES_QUARTZ)){b=10;o=3;}
+            for (int i = 0;i<=rnd.nextInt(o,b);i++) {
+                pPlayer.addItem(ore_to_item(item_name));
+
             }
         }
             return InteractionResult.SUCCESS;
@@ -47,6 +57,28 @@ public class ExtractorBlock extends Block{
     public void appendHoverText(ItemStack pStack, @Nullable BlockGetter pLevel, List<Component> pTooltip, TooltipFlag pFlag) {
         pTooltip.add(Component.translatable("tooltip.playground.extractor.tooltip"));
         super.appendHoverText(pStack, pLevel, pTooltip, pFlag);
+    }
+    private static ItemStack ore_to_item(String name){
+
+        switch (name){
+            case "block.playground.jade_ore_block":
+                return ModItems.JADE.get().getDefaultInstance();
+            case "block.minecraft.diamond_ore":
+                return Items.DIAMOND.getDefaultInstance();
+            case "block.minecraft.deepslate_diamond_ore":
+                return Items.DIAMOND.getDefaultInstance();
+            case "block.minecraft.nether_quartz_ore":
+                return Items.QUARTZ.getDefaultInstance();
+            case "block.minecraft.lapis_ore":
+                return Items.LAPIS_LAZULI.getDefaultInstance();
+            case "block.minecraft.deepslate_lapis_ore":
+                return Items.LAPIS_LAZULI.getDefaultInstance();
+            case "block.minecraft.emerald_ore":
+                return Items.EMERALD.getDefaultInstance();
+            case "block.minecraft.deepslate_emerald_ore":
+                return Items.EMERALD.getDefaultInstance();
+        }
+        return  null;
     }
 }
 
---
+++ mcmod/src/main/java/net/dread/playGround/tags/ModTags.java
@@ -0,0 +1,30 @@
+package net.dread.playGround.tags;
+
+import net.dread.playGround.playGround;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+
+public class ModTags {
+
+    public static class Blocks{
+
+
+        private static TagKey<Block> tag(String name){
+            return BlockTags.create(new ResourceLocation(playGround.MODID, name));
+        }
+    }
+
+    public static class Items{
+
+        public static final TagKey<Item> EXTRACTOR_INPUT = tag("extractor_input");
+
+
+        private static TagKey<Item> tag(String name){
+            return ItemTags.create(new ResourceLocation(playGround.MODID, name));
+        }
+    }
+}
